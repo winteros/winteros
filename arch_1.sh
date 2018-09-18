83c449088c38beb9b76eea147e662a5d93e91f8b
winteros <43388725+winteros@users.noreply.github.com>
2018-09-19 02:13:12 +0600

    Update arch_1.sh

--- arch_1.sh
+++ arch_1.sh
@@ -34,5 +34,5 @@ read -p "Пауза 3 ceк." -t 3
 mount -o bind /dev /mnt/dev
 mount -t proc none /mnt/proc
 pacman -S wget --noconfirm
-wget https://github.com/ordanax/arch.git
-sh arch_1a.sh
+wget https://github.com/winteros/arch_2.sh
+sh arch_2.sh
